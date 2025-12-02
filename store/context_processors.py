84575ace3510507c21a2429c54bc1e7ad0874b64
amine <aminemajdoub700@gmail.com>
2025-12-02 17:54:12 +0100

    Remove debug_toolbar from all files

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,12 +1,25 @@
-from .models import Wishlist, Product
+from .models import Wishlist, Product, Category
+
+def categories(request):
+    """
+    Context processor to make categories available in all templates
+    """
+    try:
+        return {'categories': Category.objects.all()}
+    except Exception as e:
+        # If there's an error (like database not ready), return empty
+        return {'categories': []}
 
 def wishlist_count(request):
+    """
+    Context processor for wishlist count
+    """
     # Authenticated: count from DB
     if request.user.is_authenticated:
         wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
         count = wishlist.products.count()
-    # Guest: count from session
     else:
+        # Guest: count from session
         product_ids = request.session.get('wishlist', [])
         count = len(product_ids)
     return {'wishlist_count': count}
