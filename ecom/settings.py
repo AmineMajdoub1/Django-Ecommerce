7966c46177bd3769a2b62bf8918711fbc5f13bab
amine <aminemajdoub700@gmail.com>
2025-12-03 14:14:56 +0100

    Remove duplicate Google ready setup

--- ecom/settings.py
+++ ecom/settings.py
@@ -93,6 +93,10 @@ TEMPLATES = [
         },
     },
 ]
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
